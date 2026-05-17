f40f001c72c9df2cbf7e37faab5c2628ec31eae9
WaveYo-Dev <dev@waveyo.cn>
2026-05-17 17:13:34 +0800

    chore: clean temp script

--- runtest.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set GOCACHE=C:\Users\diwei\go-build-cache
-mkdir C:\Users\diwei\go-build-cache 2>nul
-cd /d D:\GitHub\Source_Codes2\YoMirrorSite
-go test -short -count=1 ./internal/config/... ./internal/model/... ./internal/syncer/... ./internal/core/s3/... ./api/handler/...
